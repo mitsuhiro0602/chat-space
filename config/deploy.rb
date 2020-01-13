5c05ce20fcd59066b5afb1f7252d62561fdc0ba5
mitsuhiro0602 <0hornet0@gmail.com>
2020-01-13 22:13:07 +0900

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,10 +3,10 @@
 lock '3.11.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'ChatSpace'
+set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:mitsuhiro0602/ChatSpace.git'
+set :repo_url,  'git@github.com:mitsuhiro0602/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,10 +4,10 @@ app_path = File.expand_path('../../../../', __FILE__)
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/chat-space/current"
+working_directory "#{app_path}/current"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/chat-space/shared/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
 listen "#{app_path}/chat-space/shared/tmp/sockets/unicorn.sock"
